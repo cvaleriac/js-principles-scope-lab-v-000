6e53f74c6cd25b62285de1adaa21cc9a6d46246c
valeria campilongo <cvaleriac@hotmail.it>
2019-05-14 20:04:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var customerName = 'bob';
 
+
 function upperCaseCustomerName () {
   return customerName = customerName.toUpperCase() ;
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
 }
